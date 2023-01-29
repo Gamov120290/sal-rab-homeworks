acd561123ccdccb971ba43f0c8986c9caa89e1a6
Sergey Gamov <gamoff4@yandex.ru>
2023-01-29 18:58:18 +0300

    task 4 final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -48,7 +48,9 @@ function sendRequest(name, phone, address, goods, sum) {
     for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
-    
+    data.client = name + " " + phone;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", "+ address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
+    data.order.sum = sum;
     let jsonData = JSON.stringify({data});
 
     return jsonData;
